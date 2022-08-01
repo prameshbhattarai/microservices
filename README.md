f3d0a37938d7de083272aa4f65a89c9b2a0657a9
prameshbhattarai <pra4mesh@gmail.com>
2022-08-01 17:37:22 +0200

    * Some code cleanup and added readme file.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # spring-boot microservices
+* We have four microservices in this project.
+* All the microservices are registered in Eureka Server.
+* Each microservice invoke another service using Feign Client.
+* We have use Spring cloud gateway, each microservices request goes through this gateway.
+* Spring cloud gateway is registered in Eureka Server.
+
+
+### Tools and Technologies
+* Java 8
+* Spring boot
+* Microservices
+* Eureka Server
+* Feign Client
+* Spring Cloud Gateway
+* Spring Data JPA
+* Gradle
--- user-service/src/main/java/com/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/userservice/UserServiceApplication.java
@@ -3,9 +3,7 @@ package com.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -14,16 +12,6 @@ public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
-    }
-    
-    @Bean
-    public CommonsRequestLoggingFilter requestLoggingFilter() {
-        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
-        loggingFilter.setIncludeClientInfo(true);
-        loggingFilter.setIncludeQueryString(true);
-        loggingFilter.setIncludePayload(true);
-        loggingFilter.setIncludeHeaders(false);
-        return loggingFilter;
     }
 
 }
